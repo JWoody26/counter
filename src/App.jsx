d37a7c35d223d100c78e6ee4bb2b4e9105874c50
James Woody <james.woody.26@gmail.com>
2022-11-28 20:54:21 -0500

    Input Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useRef, useState } from 'react'
 import './App.css'
 
 
 
 function App(){
-  let number  = 0;
-  const [counter, setCounter] = useState(number);
-  const [name, setName] = useState("Name Unknown")
+
+  const [initalForm, submitForm] = useState(["Email","First Name","Last Name",]);
+
+  const TheEmailValueHere = useRef("")
+  const FirstNameValueHere = useRef("")
+  const LastNameValueHere = useRef("")
 
   return (
     <div className="App">
+      
 
-      <button onClick={function(){setCounter(function(prevCounter){
-       return prevCounter - 1
-      })}}> - </button>
-      <h1>{counter}</h1>
-      <button onClick={function(){setCounter(function(prevCounter){
-       return prevCounter + 1
-      })}}> + </button>
+<h1>Form</h1>
+      <label htmlFor="email">Your email address</label>
+      <br />
+      <input type="text" ref = {TheEmailValueHere}  name="email" />
+      <br />
+
+      <label htmlFor="first-name">Your firstname : </label>
+      <br />
+      <input type="text"ref = {FirstNameValueHere}  name="first-name" />
+      <br />
+
+      <label htmlFor="last-name">Your last name : </label>
+      <br />
+      <input type="text" ref = {LastNameValueHere} name="last-name" />
+
+      <br />
+      <br />
+
+      <button onClick={() => { submitForm([TheEmailValueHere.current.value, FirstNameValueHere.current.value, LastNameValueHere.current.value]) }}>Submit the form</button>
+      <br /> 
+      <br />
+
+      <div>
+        Your email adress is : {initalForm[0]} <br />
+        Your first name : {initalForm[1]} <br />
+        Your last name : {initalForm[2]}
+      </div>
+
     </div>
-  )
+  );
+
 }
 
 export default App
